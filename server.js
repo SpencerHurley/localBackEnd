c016c4de8e43d8db673f898553b218d63b89855d
Spencer Hurley <spencer@Spencers-MacBook-Air.local>
2018-06-25 03:38:15 -0400

    do it

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
+        "https://runningappwebdev2018.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
@@ -30,4 +30,4 @@ require('./services/run.service.server')(app);
 require('./services/team.service.server')(app);
 require('./services/segment.service.server')(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
